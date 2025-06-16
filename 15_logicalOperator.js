58f0aa9072ecb0abf020d53f3a89f649d3b3e9df
milan881 <milanraja881@gmail.com>
2025-06-16 22:34:08 +0530

    Day_05

---
+++ 15_logicalOperator.js
@@ -0,0 +1,31 @@
+/**
+ * lesson 15: Logical Operators
+ * Logical operators are used to combine multiple boolean expressions or values.
+ * They return a boolean value based on the evaluation of the expressions.
+ * Common logical operators include AND (&&), OR (||), and NOT (!).
+ * * AND (&&): Returns true if both operands are true.
+ * * OR (||): Returns true if at least one operand is true.
+ * * * NOT (!): Returns true if the operand is false, and false if the operand is true.
+ */
+
+/**
+ * And Operator (&&)
+ * Or Operator (||)
+ * Not Operator (!)
+ * Nullish Coalescing Operator (??)
+ */
+
+
+// && (AND Operator)
+
+const physics = 90;
+const chemistry = 88;
+const math = 95;
+const biology = 96;
+
+if(physics > 85 && maths > 85 && chemistry > 85 && biology > 85) {
+    console.log("You are eligible for the scholarship");
+}
+} else {
+    console.log("You are not eligible for the scholarship");
+}
